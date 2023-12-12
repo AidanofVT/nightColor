d50b86fa24c871f53d04a8288040dacf9092be2d
aidan <aidanofvt@gmail.com>
2023-12-12 09:23:01 -0500

    Fixed service pointing to wrong script. Fixed script using error-causing time format. Added .gitignore.

--- nightColor.sh
+++ nightColor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if  [ $(( $(date +%H) % 19 )) -lt 6 ]; then
+if  [ $(( $(date +%-H) % 19 )) -lt 6 ]; then
 	plasma-apply-colorscheme BreezeDark -platform offscreen
 #	/usr/lib/plasma-changeicons Colorful-Dark-Icons
 else
